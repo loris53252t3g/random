d3aa556812daf28b22ed31339fe257f717e40e24
lorisholder <113813846+lorisholder@users.noreply.github.com>
2022-10-14 21:57:08 -0400

    Create nosslockuserid.lua

---
+++ backup/nosslockuserid.lua
@@ -0,0 +1,41 @@
+--// WHO CAN EDIT: Nosss, Atman, Kacper, Snuskebazn
+
+--// SILENT-AIM Users Whitelist
+
+
+
+--[[ 
+
+	Attemping to Edit Anything below this point will not impact the outcome of the script and you will get logged and blacklisted instantly.
+		
+	Finding this raw file doesn't prove anything to you or your little friends so just be aware of that!
+
+--]]
+
+
+
+---everything above this is noss words 🤡🤡🤡🤡
+
+
+_G.SilentVIP = {
+
+	--// Admins & Owners
+	237149627; -- LORIS BITCHHH
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+}
