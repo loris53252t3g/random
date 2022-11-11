2fc3fe945dd6dd0696e5ffc46d20fbe731d7c291
lorisholder <113813846+lorisholder@users.noreply.github.com>
2022-11-11 14:16:49 -0500

    Add files via upload

---
+++ speed websites getting scraped/assets/js/typed.js
@@ -0,0 +1,9 @@
+new Typed('#typed',{
+    strings : [,'Money lover','Drug Addict','crypto mastermind','immortal',],
+    typeSpeed : 40,
+    delaySpeed : 90,
+    showCursor: false,
+    loop : true
+})
+
+
