5a7a120033b5996cb39d44561063161d8b4edb0a
lorisholder <113813846+lorisholder@users.noreply.github.com>
2022-11-11 21:15:58 -0500

    Delete speed websites getting scraped directory

--- speed websites getting scraped/assets/js/typed.js
+++
@@ -1,9 +0,0 @@
-new Typed('#typed',{
-    strings : [,'Money lover','Drug Addict','crypto mastermind','immortal',],
-    typeSpeed : 40,
-    delaySpeed : 90,
-    showCursor: false,
-    loop : true
-})
-
-
