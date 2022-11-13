cbe4187abd955179f614ad8410d089d12b205f27
lorisholder <113813846+lorisholder@users.noreply.github.com>
2022-11-12 19:51:34 -0500

    Update eclipse desync.lua

--- vault/eclipse desync.lua
+++ vault/eclipse desync.lua
@@ -18,3 +18,10 @@ end)
 ---hvh
 
 
+game:GetService("RunService").heartbeat:Connect(function()
+    local vel = game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(5343733,31453636,5436726) --random shit for hvh
+    game:GetService("RunService").RenderStepped:Wait()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = vel
+    end 
+end)
