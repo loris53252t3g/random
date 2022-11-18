4b48e9f1409a30556c2efb884075b54018f4316d
lorisholder <113813846+lorisholder@users.noreply.github.com>
2022-11-18 17:25:05 -0500

    Update eclipse desync.lua

--- vault/eclipse desync.lua
+++ vault/eclipse desync.lua
@@ -3,7 +3,10 @@
  --// i did this with roblox studios might be off
 ---sky
 
+local toggle = true
+
 game:GetService("RunService").heartbeat:Connect(function()
+if toggle then
     local vel = game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity
     game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,810,0) 
     game:GetService("RunService").RenderStepped:Wait()
@@ -12,7 +15,9 @@ game:GetService("RunService").heartbeat:Connect(function()
 end)
 
 --- underground
+local toggle = true
 game:GetService("RunService").heartbeat:Connect(function()
+if toggle then
     local vel = game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity
     game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,-810,0) 
     game:GetService("RunService").RenderStepped:Wait()
@@ -24,8 +29,9 @@ end)
 
 ---hvh
 
-
+local toggle = true
 game:GetService("RunService").heartbeat:Connect(function()
+if toggle then
     local vel = game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity
     game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(1,1,1) 
     game:GetService("RunService").RenderStepped:Wait()
